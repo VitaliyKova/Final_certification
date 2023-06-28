df981f6bee6b637cd861a43803406d17d10e3295
«Vitaliy.K <dr.kovalyk2017@yandex.ru>
2023-06-28 19:33:41 +0300

    new method

--- Final work/Array/Program.cs
+++ Final work/Array/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+string[] ArrayСhoice(string[] ArrayText)
+{
+    int size = 0;
+    for (int i = 0; i < ArrayText.Length; i++)
+    {
+        int count = 0;
+        foreach (char c in ArrayText[i])
+        {
+            count++;
+        }
+        if (count <= 3)
+            size++;
+    }
+}
