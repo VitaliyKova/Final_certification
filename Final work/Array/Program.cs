01c18590c316e4a035fd7fce70bd5eaa768872fb
«Vitaliy.K <dr.kovalyk2017@yandex.ru>
2023-06-28 20:17:18 +0300

    Finish

--- Final work/Array/Program.cs
+++ Final work/Array/Program.cs
@@ -1,4 +1,6 @@
-﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 Примеры:
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
@@ -6,9 +8,24 @@
 [“Russia”, “Denmark”, “Kazan”] → []
 */
 
+// РЕШЕНИЕ
+
+string[] NewArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} слово");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
+
+
 string[] ArrayСhoice(string[] ArrayText)
 {
-    int size = 0;
+    string[] NewArray = new string[ArrayText.Length];
+    int index = 0;
     for (int i = 0; i < ArrayText.Length; i++)
     {
         int count = 0;
@@ -17,6 +34,20 @@ string[] ArrayСhoice(string[] ArrayText)
             count++;
         }
         if (count <= 3)
-            size++;
+        {
+            NewArray[index] = ArrayText[i];
+            index++;
+        }
+
     }
+    return NewArray;
 }
+
+Console.WriteLine("Укажите колличество слов");
+int size = int.Parse(Console.ReadLine()!);
+
+string[] Myarray = NewArray(size);
+string[] ArrayChoise = ArrayСhoice(Myarray);
+
+Console.WriteLine($"[{string.Join(" ", ArrayChoise)}]");
+
